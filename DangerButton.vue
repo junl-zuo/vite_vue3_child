ada05d5417aa323f5ca8d69d07fdd99a1ac1addb
junlan.zuo <983835208@qq>
2026-03-27 14:15:32 +0800

    feat：新增项目

---
+++ DangerButton.vue
@@ -0,0 +1,15 @@
+<template>
+  <el-button type="danger" @click="handleClick">
+    <slot>Danger Button</slot>
+  </el-button>
+</template>
+
+<script setup>
+const handleClick = () => {
+  console.log('Danger button clicked');
+};
+</script>
+
+<style scoped>
+/* Add any custom styles here */
+</style>
