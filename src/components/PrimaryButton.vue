7c66705639c51cb82f16233302c4c5e4fd8bd170
junlan.zuo <983835208@qq>
2026-03-27 14:17:32 +0800

    fix：新建项目

---
+++ src/components/PrimaryButton.vue
@@ -0,0 +1,15 @@
+<template>
+  <el-button type="primary" @click="handleClick">
+    <slot>Primary Button</slot>
+  </el-button>
+</template>
+
+<script setup>
+const handleClick = () => {
+  console.log('Primary button clicked');
+};
+</script>
+
+<style scoped>
+/* Add any custom styles here */
+</style>
